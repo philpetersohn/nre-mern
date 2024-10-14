4e6c71b7c8f15108bd6a3bd278db10c11675215e
philpetersohn <petersohn.phil@gmail.com>
2024-10-14 15:21:50 -0700

    Implemented middleware and error handler middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import "dotenv/config";
-import { db } from "./libs/dbConnect.js";
 
 import userRouter from "./routes/user.route.js";
+import { errorHandler } from "./libs/middleware.js";
 
 const PORT = 8000;
 const app = express();
@@ -15,7 +15,8 @@ app.use("*", (req, res) => {
   res.status(404).json({ message: "not found" });
 });
 
-// Start the Express server
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
